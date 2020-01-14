58847c5e5b095c80072725ea514a191f51b90d95
apezkin <ape.zkin@gmail.com>
2020-01-14 15:02:39 +0200

    Styling & localization

--- reactproject/src/Components/Item.js
+++ reactproject/src/Components/Item.js
@@ -7,14 +7,14 @@ function Item(props) {
         <div className="item-bg">
             <h4>{props.name}</h4>
             <div>
-                <h4>Hinta</h4>
-                <h4 className="title">{props.price}</h4>
+                <h4>Price</h4>
+                <h4 className="title">{props.price}€</h4>
             </div>
             <div>
-                <h4>Varastossa</h4>
+                <h4>Inventory</h4>
                 <h4 className="title">{props.inventory}</h4>
             </div>
-            <button className="buy-button" onClick={() => props.buy(props.id)}>Osta</button>
+            <button className="buy-button" onClick={() => props.buy(props.id)}>Buy</button>
         </div>
     );
 }
