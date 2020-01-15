1d00b615f76ea8c74553a490c0d4e524dea77872
apezkin <ape.zkin@gmail.com>
2020-01-15 15:24:36 +0200

    bootstrap

--- reactproject/src/Components/Menu.js
+++ reactproject/src/Components/Menu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ItemList from "./ItemList"
 import UserInfo from "./UserInfo"
 import {Link} from "react-router-dom";
-import  'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Menu extends React.Component {
     state = {
